813c0b6e0e168fbf5115ac935edfd136d0b39a69
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-04-13 07:16:28 +0000

    rails-show

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get 'lists/new'
   post 'lists/new'
-  get 'lists/new'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  
-  
-  
-  
-  
-  
+ get 'lists/lists'=>'lists#index'
+
+
+  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
+
   post 'lists' => 'lists#create'
   get 'lists/index'
   get 'lists/show'
